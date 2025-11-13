1f410de72e8cef030f94edea62c77008044c1683
tuokaikyle <kaituo@outlook.com>
2025-11-14 00:20:34 +1000

    region names

---
+++ province-map/index.ts
@@ -0,0 +1,27 @@
+import { join } from 'path';
+
+// https://datav.aliyun.com/portal/school/atlas/area_selector - no MIT license
+// const inputPath = join(import.meta.dir, './input/datav-province.json');
+// const outputPath = join(import.meta.dir, './output/processed-province.json');
+
+// const data = await Bun.file(inputPath).json();
+
+// const processed = data.features.map(f => ({name: f.properties.name, adcode: f.properties.adcode}) )
+
+
+const inputPath = join(import.meta.dir, './input/datav-city.json');
+const outputPath = join(import.meta.dir, './output/processed-city.json');
+
+const data = await Bun.file(inputPath).json();
+
+const processed = data.features.map(f => {
+  const properties = f.properties;
+  return {
+    name: properties.name,
+    adcode: properties.adcode,
+    parent: properties?.parent?.adcode ?? '',
+
+  };
+})
+
+await Bun.write(outputPath, JSON.stringify(processed, null, 2));
